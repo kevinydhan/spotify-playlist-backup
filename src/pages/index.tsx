add305610475a628351611ec08d72baec172a94c
Kevin Han <kevinhanyd@gmail.com>
2021-02-25 11:52:17 -0500

    chore: add stitches as dependency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,64 +9,6 @@ interface IndexPageProps extends PageProps {
   playlists: SpotifyApi.PlaylistObjectSimplified[]
 }
 
-const downloadPlaylistBackup = async (
-  playlist: SpotifyApi.PlaylistObjectSimplified,
-  trackUris: SpotifyApi.TrackObjectSimplified['uri'][]
-): Promise<void> => {
-  console.log({ trackUris: trackUris.length })
-  const playlistDetails = {
-    name: playlist.name,
-    description: playlist.description,
-    public: playlist.public,
-    collaborative: playlist.collaborative,
-    uris: trackUris,
-    createdAt: new Date().toISOString(),
-  }
-  const data = JSON.stringify(playlistDetails, null, 2)
-
-  /**
-   * @see https://stackoverflow.com/a/33542499/11715889
-   */
-  const blob = new Blob([data], { type: 'text/json' })
-  if (window?.navigator?.msSaveOrOpenBlob) {
-    window.navigator.msSaveOrOpenBlob(blob, 'playlist.json')
-  } else {
-    const elem = window.document.createElement('a')
-    elem.href = window.URL.createObjectURL(blob)
-    elem.download = 'playlist.json'
-    document.body.appendChild(elem)
-    elem.click()
-    document.body.removeChild(elem)
-  }
-}
-
-const getPlaylistTrackUris = async ({
-  accessToken,
-  playlistId,
-}: {
-  playlistId: string
-  accessToken: string
-}) => {
-  spotify.setAccessToken(accessToken)
-
-  const playlistTracks: SpotifyApi.PlaylistTrackObject[] = []
-  let response = await spotify.getPlaylistTracks(playlistId)
-
-  playlistTracks.push(...response.body.items)
-
-  while (response.body.next) {
-    response = await spotify.getPlaylistTracks(playlistId, {
-      limit: response.body.limit,
-      offset: response.body.offset + response.body.limit,
-    })
-    playlistTracks.push(...response.body.items)
-  }
-
-  return playlistTracks
-    .filter((track) => !track.is_local)
-    .map(({ track }) => track.uri)
-}
-
 const IndexPage: NextPage<IndexPageProps> = ({ session, playlists }) => {
   if (!session) return <a href="/api/auth/signin">Log in to Spotify</a>
   return (
@@ -79,20 +21,6 @@ const IndexPage: NextPage<IndexPageProps> = ({ session, playlists }) => {
             {...playlist}
             accessToken={session?.accessToken}
           />
-          // <li key={playlist.id}>
-          //   {playlist.name}
-          //   <button
-          //     onClick={async () => {
-          //       const trackUris = await getPlaylistTrackUris({
-          //         accessToken: session.accessToken,
-          //         playlistId: playlist.id,
-          //       })
-          //       await downloadPlaylistBackup(playlist, trackUris)
-          //     }}
-          //   >
-          //     Download playlist
-          //   </button>
-          // </li>
         ))}
       </ul>
     </div>
