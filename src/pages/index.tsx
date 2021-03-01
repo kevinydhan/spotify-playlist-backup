47d4fad61e1b7989c4a87a748d67e75a53617643
Kevin Han <kevinhanyd@gmail.com>
2021-03-01 14:34:20 -0500

    feat: update index page styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
 import type { GetServerSideProps, NextPage } from 'next'
 import Image from 'next/image'
-import { getProviders } from 'next-auth/client'
+import { getProviders, SessionProvider, signIn } from 'next-auth/client'
 
 import type { CommonPageProps } from '@/typings/pages'
 
+import buttonStyles from '../styles/components/_button.module.scss'
+import pageStyles from '../styles/pages/home.module.scss'
+
 type IndexPageProps = CommonPageProps
 
-const IndexPage: NextPage<IndexPageProps> = () => (
-  <div>
+const IndexPage: NextPage<IndexPageProps> = ({ provider }) => (
+  <div className={pageStyles.root}>
+    <div>
+      <h1 className={pageStyles.heading}>Spotify Playlist Backup</h1>
+      <p className={pageStyles.description}>
+        Download backup files for your Spotify playlists which can then be used
+        to recreate your playlists
+      </p>
+      <button
+        className={buttonStyles.button}
+        onClick={handleSignOutButtonClick(provider?.id)}
+      >
+        Sign in with {provider?.name}
+      </button>
+    </div>
     <div>
       <Image
         src="/images/landing-hero.svg"
@@ -16,22 +32,13 @@ const IndexPage: NextPage<IndexPageProps> = () => (
         height="518.69685"
       />
     </div>
-    <div>
-      <h1
-        style={{
-          fontSize: '36px',
-          lineHeight: '1.2',
-        }}
-      >
-        Spotify Playlist Backup
-      </h1>
-      <p>
-        Download backup files for your Spotify playlists which can then be used
-        to recreate your playlists
-      </p>
-    </div>
   </div>
 )
+
+const handleSignOutButtonClick = (providerId: SessionProvider['id']) => () => {
+  const callbackUrl = `${window?.location?.origin}/dashboard`
+  signIn(providerId, { callbackUrl })
+}
 
 export const getServerSideProps: GetServerSideProps<IndexPageProps> = async () => {
   const providers = await getProviders()
