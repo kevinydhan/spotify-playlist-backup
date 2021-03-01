4217a6b574157b3d7dfee0cf95f1700ebb7290bf
Kevin Han <kevinhanyd@gmail.com>
2021-03-01 14:19:28 -0500

    fix: remove @stitches/react from index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,48 +2,35 @@ import type { GetServerSideProps, NextPage } from 'next'
 import Image from 'next/image'
 import { getProviders } from 'next-auth/client'
 
-import { Box, Heading } from '@/theme/components'
 import type { CommonPageProps } from '@/typings/pages'
 
 type IndexPageProps = CommonPageProps
 
 const IndexPage: NextPage<IndexPageProps> = () => (
-  <Box
-    css={{
-      marginTop: '20px',
-      minHeight: 'calc(100vh - 80px)',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-    }}
-  >
-    <Box>
+  <div>
+    <div>
       <Image
         src="/images/landing-hero.svg"
         alt="Girl standing next to a stack of windows with a play button"
         width="886.10205"
         height="518.69685"
       />
-    </Box>
-    <Box>
-      <Heading
-        css={{
+    </div>
+    <div>
+      <h1
+        style={{
           fontSize: '36px',
           lineHeight: '1.2',
-
-          tablet0: {
-            fontSize: '42px',
-          },
         }}
       >
         Spotify Playlist Backup
-      </Heading>
+      </h1>
       <p>
         Download backup files for your Spotify playlists which can then be used
         to recreate your playlists
       </p>
-    </Box>
-  </Box>
+    </div>
+  </div>
 )
 
 export const getServerSideProps: GetServerSideProps<IndexPageProps> = async () => {
